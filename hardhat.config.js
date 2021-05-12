334dc994abe8a3433471c4829f125655ebcd23ca
Barry Lavides <lavidesbarry@gmail.com>
2021-05-12 21:50:39 +0800

    - Added INIT_CODE_PAIR_HASH in UniswapV2Factory.sol - Added localhost network in hardhat config - Trim down hardhat deployment script to just the factory deployment and display of INIT_CODE_PAIR_HASH

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,6 +10,9 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.5.16",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
     kovan: {
       url: INFURA_URL,
       accounts: [`0x${PRIVATE_KEY}`]
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,24 +6,8 @@ async function main() {
     const FactoryContract = await Factory.deploy(deployer.address);
     const factory = await FactoryContract.deployed();
 
-    let token1Address, token2Address;
-
-    const Token1 = await ethers.getContractFactory('Token1');
-    const Token1Contract = await Token1.deploy();
-    const token1 = await Token1Contract.deployed();
-
-    const Token2 = await ethers.getContractFactory('Token2');
-    const Token2Contract = await Token2.deploy();
-    const token2 = await Token2Contract.deployed();
-
-    token1Address = token1.address;
-    token2Address = token2.address;
-
-    await factory.createPair(token1Address, token2Address);
-
     console.log('Factory Address: ', factory.address);
-    console.log('Token 1 Address: ', token1Address);
-    console.log('Token 2 Address: ', token2Address);
+    console.log('INIT_CODE_PAIR_HASH: ', await factory.INIT_CODE_PAIR_HASH());
   }
   
 main()
